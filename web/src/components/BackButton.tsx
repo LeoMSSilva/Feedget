ebfb9c2986ba927048bd0672660844b8909fd5ec
LeoMSSilva <l.eom1994@hotmail.com>
2022-05-03 14:09:09 -0300

    Feature: created backButton component

---
+++ web/src/components/BackButton.tsx
@@ -0,0 +1,20 @@
+import { ArrowLeft } from 'phosphor-react';
+
+export interface IBackButton {
+  goBack: () => void;
+}
+
+export const BackButton = ({ goBack }: IBackButton) => {
+  return (
+    <button
+      className="absolute top-5 left-5 text-zinc-400 hover:text-zinc-100 transition-all duration-500 ease-linear"
+      onClick={() => goBack()}
+      type="button"
+    >
+      <ArrowLeft
+        className="w-4 h-4"
+        weight="bold"
+      />
+    </button>
+  );
+};
