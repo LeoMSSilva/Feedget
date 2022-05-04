0dc94d090163be4267204696bad6152161453ce1
LeoMSSilva <l.eom1994@hotmail.com>
2022-05-04 13:29:29 -0300

    Feature: implemented sending mails

--- server/src/routers.ts
+++ server/src/routers.ts
@@ -1,4 +1,5 @@
 import { prisma } from './prisma';
+import { transport } from './nodemailer';
 import express from 'express';
 
 export const routes = express.Router();
@@ -9,5 +10,18 @@ routes.post('/feedbacks', async (req, res) => {
 	const feedback = await prisma.feedback.create({
 		data: { type, comment, screenshot },
 	});
+
+	await transport.sendMail({
+		from: 'Equipe Feedget <oi@feedget.com>',
+		to: 'LeoMSSilva <leo@test.com>',
+		subject: 'Novo feedback',
+		html: [
+			`<div style="font-family: sans-serif; font-size:16px; color: #111;">`,
+			`<p>Tipo do feedback: ${type}</p>`,
+			`<p>Comentário: ${comment}</p>`,
+			`</div>`,
+		].join('\n'),
+	});
+
 	return res.status(201).json({ data: feedback });
 });
