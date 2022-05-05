7a983a516fa5db866151875c17f505f533e1117e
LeoMSSilva <l.eom1994@hotmail.com>
2022-05-05 11:34:05 -0300

    Feature: installed and configured expo-app-loading, expo-font and @expo-google-fonts/inter libs

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,33 @@
+import AppLoading from 'expo-app-loading';
+import {
+	useFonts,
+	Inter_400Regular,
+	Inter_500Medium,
+} from '@expo-google-fonts/inter';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { theme } from './src/theme';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_500Medium,
+	});
+
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
+
+	return (
+		<View style={styles.container}>
+			<StatusBar style="light" backgroundColor="transparent" translucent />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: theme.colors.background,
+	},
 });
